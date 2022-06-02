ea95bd1202e79760d583c23d50a4c39831bdcf58
Dias Kereibay <dkereiba@cern.ch>
2022-06-02 16:43:44 +0300

    KurganDetectorConstruction.cc was changed

--- src/KurganDetectorConstruction.cc
+++ src/KurganDetectorConstruction.cc
@@ -29,7 +29,6 @@ using namespace std;
 KurganDetectorConstruction::KurganDetectorConstruction()
 : G4VUserDetectorConstruction(),
   fSensetiveDetector(nullptr),
-  fSensetiveDetector2(nullptr),
   fScoringVolume(nullptr)
 { }
 
@@ -47,10 +46,8 @@ G4VPhysicalVolume* KurganDetectorConstruction::Construct()
 
   //////////////////////////////*** WORLD ***//////////////////////////////
   
-  //G4double world_sizeXY = 750.*cm;	//1.2*env_sizeXY;
-  //G4double world_sizeZ  = 750.*cm;	//1.2*env_sizeZ;
-  G4double world_sizeXY = 200.*m;	//1.2*env_sizeXY;
-  G4double world_sizeZ  = 200.*m;	//1.2*env_sizeZ;
+  G4double world_sizeXY = 750.*cm;	//1.2*env_sizeXY;
+  G4double world_sizeZ  = 750.*cm;	//1.2*env_sizeZ;
   G4Material* world_mat = nist->FindOrBuildMaterial("G4_AIR");
   
   G4Box* solidWorld =    
@@ -90,10 +87,8 @@ G4VPhysicalVolume* KurganDetectorConstruction::Construct()
 
   //////////////////////////////*** ENVELOPE ***//////////////////////////////
   
-  //G4double env_sizeXY = 600. * cm; 
-  //G4double env_sizeZ = 600. * cm;
-  G4double env_sizeXY = 200.*m; 
-  G4double env_sizeZ = 200.*m;
+  G4double env_sizeXY = 600. * cm; 
+  G4double env_sizeZ = 600. * cm;
   G4Material* env_mat = Vacuum;
 
   G4Box* solidEnv =    
@@ -125,6 +120,7 @@ G4VPhysicalVolume* KurganDetectorConstruction::Construct()
    G4double density_Ar = 0.001784*g/cm3; 
    G4Material* Ar = nist->FindOrBuildMaterial("G4_Ar"); // density = 0.00166201(g/cm3)
        		
+
 			/////***Gas in straw tubes***/////  
    // 30% CO2 + 70% Ar
    G4double density_gas = 0.3 * density_CO2 + 0.7 * density_Ar;//0.001848*g/cm3
@@ -148,95 +144,6 @@ G4VPhysicalVolume* KurganDetectorConstruction::Construct()
    G4double Cu_coating = 50.0*nm;//pow(10.0,-7)*cm;
    G4double Au_coating = 20.0*nm;//pow(10.0,-7)*cm;
    
-
-  //химический состав почвы 
-/*  G4double density_O = 0.00133151*g/cm3;
-  G4double density_Si = 2.33*g/cm3;
-  G4double density_Al = 2.699*g/cm3;
-  G4double density_Fe = 7.874*g/cm3;
-  G4double density_C = 2*g/cm3;
-  G4double density_Ca = 1.55*g/cm3;
-  G4double density_K = 0.862*g/cm3;
-  G4double density_Na = 0.971*g/cm3;
-  G4double density_Mg = 1.74*g/cm3;
-  G4double density_N = 0.0011652*g/cm3;
-*/
-
-  G4Material* O = nist->FindOrBuildMaterial("G4_O");
-  G4Material* Si = nist->FindOrBuildMaterial("G4_Si");
-  G4Material* Al = nist->FindOrBuildMaterial("G4_Al");
-  G4Material* Fe = nist->FindOrBuildMaterial("G4_Fe");
-  G4Material* C = nist->FindOrBuildMaterial("G4_C");
-  G4Material* Ca = nist->FindOrBuildMaterial("G4_Ca");
-  G4Material* K = nist->FindOrBuildMaterial("G4_K");
-  G4Material* Na = nist->FindOrBuildMaterial("G4_Na");
-  G4Material* Mg = nist->FindOrBuildMaterial("G4_Mg"); 
-  G4Material* N = nist->FindOrBuildMaterial("G4_N"); 
-  
-//  G4double density_Pb = 11.35*g/cm3;
-  G4Material* Pb_mat = nist->FindOrBuildMaterial("G4_Pb"); 
-
-//  G4double density_O2 = 0.00142897*g/cm3;
-//  G4Material* O2 = new G4Material("O2", density_O2, 1);
-//  O2->AddElement(O2, 2);
-
-  G4double density_Soil = 5.51*g/cm3;
-  G4Material* Soil_mat = new G4Material("Soil", density_Soil, 10);
-  Soil_mat->AddMaterial(O, 100*perCent);
-  Soil_mat->AddMaterial(Si, 33*perCent);
-  Soil_mat->AddMaterial(Al, 8.11*perCent);
-  Soil_mat->AddMaterial(Fe, 3.8*perCent);
-  Soil_mat->AddMaterial(C, 2*perCent);
-  Soil_mat->AddMaterial(Ca, 1.37*perCent);
-  Soil_mat->AddMaterial(K, 1.36*perCent);
-  Soil_mat->AddMaterial(Na, 0.63*perCent);
-  Soil_mat->AddMaterial(Mg, 0.63*perCent);
-  Soil_mat->AddMaterial(N, 0.1*perCent);
-
-/*  G4double density_P = 2.2*g/cm3;
-
-  G4double density_OxideKalium = 2.32*g/cm3;
-  G4double density_OxidePhosfor = 2.39*g/cm3;
-  G4Material* K = nist->FindOrBuildMaterial("G4_K");
-  G4Material* O = nist->FindOrBuildMaterial("G4_O");
-  G4Material* P = nist->FindOrBuildMaterial("G4_P") 
-  G4Material* N = nist->FindOrBuildMaterial("G4_N") 
-
-
-  G4Material* K2O = new G4Material("OxideKalium", density_OxideKalium, 2); 
-  K2O->AddElement(K, 2);
-  K2O->AddElement(O, 1);
-  
-  G4Material* P2O5 = new G4Material("OxidePhosfor", density_OxidePhosfor, 2);
-  P2O5->AddElement(P, 2);
-  P2O5->AddElement(O, 5);
-
-  G4double density_Soil = 5.51*g/cm3;
-  G4Material* Soil = new G4Material("Soil", density_Soil, 3);
-  Soil->AddMaterial(K2O, 30*perCent);
-  Soil->AddMaterial(P2O5, 30*perCent);
-  Soil->AddMaterial(N, 30*perCent);
-*/
-
-   G4double high_Soil = 100.*cm;
-   G4double width_Soil = 100.*cm;
-   G4double length_Soil = 35.*m;
-   G4Box* Kurgan_Box = new G4Box("Kurgan", width_Soil, high_Soil, length_Soil);
-    
-    //fSolidVolume
-   G4LogicalVolume* Kurgan_Volume =  new G4LogicalVolume(Kurgan_Box, /*Straw_Gas*/Pb_mat /*straw_mat_MY*/, "Kurgan");
-    
-   G4ThreeVector position_Kurgan = G4ThreeVector(0.*cm, 0.*cm, length_Soil+5.2*m);
-    new G4PVPlacement(0,                       //no rotation
-                    position_Kurgan,         //at (0,0,0)
-                    Kurgan_Volume,                //its logical volume
-                    "Kurgan_Box",              //its name
-                    logicWorld,              //its mother  volume
-                    false,                   //no boolean operation
-                    0,                       //copy number
-                    checkOverlaps);          //overlaps checking
-
-
 	//геометрические характеристики Анода
    G4double anod_Au = 10.0*nm;
    G4double D_anod = 0.02*mm;
@@ -260,8 +167,7 @@ G4VPhysicalVolume* KurganDetectorConstruction::Construct()
    G4Tubs* Straw_Inner_Gas = new G4Tubs("Straw", R_anod, Straw_R_3-0.1*mm, Straw_Dz, Straw_SPhi, Straw_DPhi);//Газ в строу трубке
    G4Tubs* Straw_Anod_Au = new G4Tubs("Straw", R_anod_W, R_anod, Straw_Dz, Straw_SPhi, Straw_DPhi);//Газ в строу трубке
    G4Tubs* Straw_Anod_W = new G4Tubs("Straw", 0.*mm, R_anod_W, Straw_Dz, Straw_SPhi, Straw_DPhi);//Газ в строу трубке  
-   //G4Box* Aperture = new G4Box("Aperture", Straw_Dz+20.*cm, Straw_Dz+20.*cm, 0.001*cm);
-   G4Box* Aperture = new G4Box("Aperture", width_Soil, high_Soil, 0.001*cm);
+   G4Box* Aperture = new G4Box("Aperture", Straw_Dz+20.*cm, Straw_Dz+20.*cm, 0.001*cm);
 	
 #ifdef SHOW_COMMENTS_DetectorConstruction_cc  
 	cout<<"Straw_outer_D="<<Straw_outer_diameter/mm<<"mm"<<endl;
@@ -314,13 +220,7 @@ G4VPhysicalVolume* KurganDetectorConstruction::Construct()
      new G4LogicalVolume(Aperture,         //its solid
                         Straw_Gas,          //its material
                         "Aperture");           //its name
-
-    //fSensetiveVolume
-   G4LogicalVolume* logicShape8 =                    
-     new G4LogicalVolume(Aperture,         //its solid
-                        Straw_Gas,          //its material
-                        "Aperture");           //its name
-    //////////////////////////////*** View X ***//////////////////////////////
+   //////////////////////////////*** View X ***//////////////////////////////
     //первая камера, состоит из 4 слоев(XXYY), строу трубок 200 трубки на слой 
     //вторая камера, состоит из 4 слоев(XXYY), строу трубок 200 трубки на слой 
     //третяя камера, состоит из 4 слоев(XXYY), строу трубок 200 трубки на слой 
@@ -349,7 +249,7 @@ G4VPhysicalVolume* KurganDetectorConstruction::Construct()
 	cout<<"N_straws_per_layer="<<N_straws_per_layer<<endl;
 #endif
 
-/*
+
    for(G4int chamber=0; chamber<N_chamber; chamber++){
     cout<<"ChamberPosition_Z = "<<ChamberPosition_Z<<endl;    
       for(G4int layer=0; layer<N_layer; layer++){ 
@@ -391,7 +291,7 @@ G4VPhysicalVolume* KurganDetectorConstruction::Construct()
 
   #ifdef SHOW_COMMENTS_DetectorConstruction_cc_test 
     G4cout<<i+N_straws_per_layer*layer+chamber*N_layer*N_straws_per_layer+1<<" "<<position_XY*cos(phi)<<" "<<position_XY*sin(phi)<<" "<<layer_position_Z<<G4endl;
-    G4cout<<rot<<" "<<rot_1<<G4endl;
+//    G4cout<<rot<<" "<<rot_1<<G4endl;
   #endif
     
       position = G4ThreeVector(position_XY*cos(phi), position_XY*sin(phi), layer_position_Z);
@@ -448,9 +348,8 @@ G4VPhysicalVolume* KurganDetectorConstruction::Construct()
 	}
   ChamberPosition_Z = ChamberPosition_Z + d_between_chamber;   
 }
-*/
 /////////////////sensetive detector////////////////  
-   G4ThreeVector position_Aperture = G4ThreeVector(0.*cm, 0.*cm, 0.2*m);
+   G4ThreeVector position_Aperture = G4ThreeVector(0.*cm, 0.*cm, 200.*cm);
 		  new G4PVPlacement(0,			 				    //rotm
 			      		position_Aperture,	 			  //position
                 logicShape7,           		  //its logical volume
@@ -460,19 +359,8 @@ G4VPhysicalVolume* KurganDetectorConstruction::Construct()
                 N_straw_tot+1,             	//copy number
                 checkOverlaps);          		//overlaps checking
 
-   G4ThreeVector position_Aperture_2 = G4ThreeVector(0.*cm, 0.*cm, 80.2*m);
-		  new G4PVPlacement(0,			 				    //rotm
-			      		position_Aperture_2,	 			  //position
-                logicShape8,           		  //its logical volume
-	        		  "Aperture",               	//its name
-                logicEnv,                		//its mother  volume
-                false,                   		//no boolean operation
-                602,             	//copy number
-                checkOverlaps);          		//overlaps checking
-
   fScoringVolume = logicShape4;
   fSensetiveDetector = logicShape7;
-  fSensetiveDetector2 = logicShape8;
  
   if(checkOverlaps){cout<<"checkOverlaps!"<<endl;}   
   return physWorld;
